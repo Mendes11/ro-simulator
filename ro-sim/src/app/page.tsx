e9abd89f86e32a4298088dada4ebc0fc10076b65
Rafael Mendes <rafaelmpb11@hotmail.com>
2024-12-10 19:47:39 -0300

    added some wireframes for attributes and simulation sections in main page

--- ro-sim/src/app/page.tsx
+++ ro-sim/src/app/page.tsx
@@ -10,86 +10,202 @@ export default function Home() {
   const [equipments, setEquipments] = useState<CharacterEquipments>({});
 
   return (
-    <div className="flex flex-row flex-wrap lg:max-w-[75%]">
-      <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
-        <CharacterEquipmentCard
-          title="Topo"
-          equippedItem={equipments.top}
-          onItemChanged={(item) => setEquipments({...equipments, top: item})}
-          onItemRemoved={() => setEquipments({...equipments, top: undefined})}
-        />
+    <div className="flex flex-row flex-wrap">
+      <div className="basis-full lg:basis-2/3 p-1">
+        <div className="text-lg border-b-2 border-black">
+          Atributos
+        </div>
+        <div className="border min-h-24 p-2 mt-1">
+          <div className="w-24 border">
+            <div className="w-auto">
+              <label className="float-left">For: </label>
+              <select className="overflow-hidden">
+              {[...Array(130).keys()].map((i) => (
+                <option key={i} value={i+1}>{i+1}</option>
+              ))}
+              </select>
+            </div>
+            <div className="">
+              <label>Agi: </label>
+              <select>
+              {[...Array(130).keys()].map((i) => (
+                <option key={i} value={i+1}>{i+1}</option>
+              ))}
+              </select>
+            </div>
+            <div className="">
+              <label>Vit: </label>
+              <select>
+              {[...Array(130).keys()].map((i) => (
+                <option key={i} value={i+1}>{i+1}</option>
+              ))}
+              </select>
+            </div>
+            <div className="w-auto">
+              <label className="left-0">Int: </label>
+              <select className="ml-auto">
+              {[...Array(130).keys()].map((i) => (
+                <option key={i} value={i+1}>{i+1}</option>
+              ))}
+              </select>
+            </div>
+            <div className="">
+              <label>Dex: </label>
+              <select>
+              {[...Array(130).keys()].map((i) => (
+                <option key={i} value={i+1}>{i+1}</option>
+              ))}
+              </select>
+            </div>
+            <div className="">
+              <label>Luk: </label>
+              <select>
+              {[...Array(130).keys()].map((i) => (
+                <option key={i} value={i+1}>{i+1}</option>
+              ))}
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className="text-lg border-b-2 border-black">
+          Equipamentos
+        </div>
+        <div className="flex flex-row flex-wrap p-2 mt-1">
+          <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
+            <CharacterEquipmentCard
+              title="Topo"
+              equippedItem={equipments.top}
+              onItemChanged={(item) => setEquipments({...equipments, top: item})}
+              onItemRemoved={() => setEquipments({...equipments, top: undefined})}
+            />
+          </div>
+          <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
+            <CharacterEquipmentCard
+              title="Meio"
+              equippedItem={equipments.mid}
+              onItemChanged={(item) => setEquipments({...equipments, mid: item})}
+              onItemRemoved={() => setEquipments({...equipments, mid: undefined})}
+            />
+          </div>
+          <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
+            <CharacterEquipmentCard
+              title="Baixo"
+              equippedItem={equipments.bottom}
+              onItemChanged={(item) => setEquipments({...equipments, bottom: item})}
+              onItemRemoved={() => setEquipments({...equipments, bottom: undefined})}
+            />
+          </div>
+          <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
+            <CharacterEquipmentCard
+              title="Mão Direita"
+              equippedItem={equipments.rightHand}
+              onItemChanged={(item) => setEquipments({...equipments, rightHand: item})}
+              onItemRemoved={() => setEquipments({...equipments, rightHand: undefined})}
+            />
+          </div>
+          <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
+            <CharacterEquipmentCard
+              title="Mão Esquerda"
+              equippedItem={equipments.leftHand}
+              onItemChanged={(item) => setEquipments({...equipments, leftHand: item})}
+              onItemRemoved={() => setEquipments({...equipments, leftHand: undefined})}
+            />
+          </div>
+          <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
+            <CharacterEquipmentCard
+              title="Armadura"
+              equippedItem={equipments.armor}
+              onItemChanged={(item) => setEquipments({...equipments, armor: item})}
+              onItemRemoved={() => setEquipments({...equipments, armor: undefined})}
+            />
+          </div>
+          <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
+            <CharacterEquipmentCard
+              title="Capa"
+              equippedItem={equipments.cloack}
+              onItemChanged={(item) => setEquipments({...equipments, cloack: item})}
+              onItemRemoved={() => setEquipments({...equipments, cloack: undefined})}
+            />
+          </div>
+          <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
+            <CharacterEquipmentCard
+              title="Botas"
+              equippedItem={equipments.shoes}
+              onItemChanged={(item) => setEquipments({...equipments, shoes: item})}
+              onItemRemoved={() => setEquipments({...equipments, shoes: undefined})}
+            />
+          </div>
+          <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
+            <CharacterEquipmentCard
+              title="Accessório Direito"
+              equippedItem={equipments.leftAccessory}
+              onItemChanged={(item) => setEquipments({...equipments, leftAccessory: item})}
+              onItemRemoved={() => setEquipments({...equipments, leftAccessory: undefined})}
+            />
+          </div>
+          <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
+            <CharacterEquipmentCard
+              title="Acessório Esquerdo"
+              equippedItem={equipments.rightAccessory}
+              onItemChanged={(item) => setEquipments({...equipments, rightAccessory: item})}
+              onItemRemoved={() => setEquipments({...equipments, rightAccessory: undefined})}
+            />
+          </div>
+        </div>
       </div>
-      <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
-        <CharacterEquipmentCard
-          title="Meio"
-          equippedItem={equipments.mid}
-          onItemChanged={(item) => setEquipments({...equipments, mid: item})}
-          onItemRemoved={() => setEquipments({...equipments, mid: undefined})}
-        />
-      </div>
-      <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
-        <CharacterEquipmentCard
-          title="Baixo"
-          equippedItem={equipments.bottom}
-          onItemChanged={(item) => setEquipments({...equipments, bottom: item})}
-          onItemRemoved={() => setEquipments({...equipments, bottom: undefined})}
-        />
-      </div>
-      <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
-        <CharacterEquipmentCard
-          title="Mão Direita"
-          equippedItem={equipments.rightHand}
-          onItemChanged={(item) => setEquipments({...equipments, rightHand: item})}
-          onItemRemoved={() => setEquipments({...equipments, rightHand: undefined})}
-        />
-      </div>
-      <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
-        <CharacterEquipmentCard
-          title="Mão Esquerda"
-          equippedItem={equipments.leftHand}
-          onItemChanged={(item) => setEquipments({...equipments, leftHand: item})}
-          onItemRemoved={() => setEquipments({...equipments, leftHand: undefined})}
-        />
-      </div>
-      <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
-        <CharacterEquipmentCard
-          title="Armadura"
-          equippedItem={equipments.armor}
-          onItemChanged={(item) => setEquipments({...equipments, armor: item})}
-          onItemRemoved={() => setEquipments({...equipments, armor: undefined})}
-        />
-      </div>
-      <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
-        <CharacterEquipmentCard
-          title="Capa"
-          equippedItem={equipments.cloack}
-          onItemChanged={(item) => setEquipments({...equipments, cloack: item})}
-          onItemRemoved={() => setEquipments({...equipments, cloack: undefined})}
-        />
-      </div>
-      <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
-        <CharacterEquipmentCard
-          title="Botas"
-          equippedItem={equipments.shoes}
-          onItemChanged={(item) => setEquipments({...equipments, shoes: item})}
-          onItemRemoved={() => setEquipments({...equipments, shoes: undefined})}
-        />
-      </div>
-      <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
-        <CharacterEquipmentCard
-          title="Accessório Direito"
-          equippedItem={equipments.leftAccessory}
-          onItemChanged={(item) => setEquipments({...equipments, leftAccessory: item})}
-          onItemRemoved={() => setEquipments({...equipments, leftAccessory: undefined})}
-        />
-      </div>
-      <div className="flex-grow basis-full md:basis-1/2 lg:basis-1/3">
-        <CharacterEquipmentCard
-          title="Acessório Esquerdo"
-          equippedItem={equipments.rightAccessory}
-          onItemChanged={(item) => setEquipments({...equipments, rightAccessory: item})}
-          onItemRemoved={() => setEquipments({...equipments, rightAccessory: undefined})}
-        />
+      <div className="w-auto basis-full lg:basis-1/3 p-1">
+        <div className="text-lg border-b-2 border-black">
+          <p>Simulação</p>
+        </div>
+        <div className="mt-1 p-2">
+          <div>
+            <label htmlFor="attackType">Habilidade: </label>
+            <select className="border-2 rounded-md p-1" name="attackType">
+              <option value="basic">Ataque Básico</option>
+              <option value="ASC_BREAKER">SBK</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="attackType">Oponente: </label>
+            <select className="border-2 rounded-md p-1" name="attackType">
+              <option value="basic">
+                Dummy
+              </option>
+            </select>
+          </div>
+          <div>
+            <ul className="mt-1 font-bold">
+              <li>Dano Habilidade = 0.0</li>
+              <li>Dano Habilidade (Crit) = 0.0</li>
+            </ul>
+          </div>
+          <div className="mt-3">
+            <p className="border-b border-black mb-2 font-bold">Informações Adicionais</p>
+            <ul className="ml-2">
+              <li>ATK = 0.0</li>
+              <li>Dano Base Corpo a Corpo = 0.0</li>
+              <li>Dano Base Corpo a Corpo (Crit) = 0.0</li>
+              <li>Dano Base Distância (Crit) = 0.0</li>
+              <p className="mt-2 inline-block border-b border-black mb-2">{"Multiplicadores"}</p>
+              <li>%Raça = 0%</li>
+              <li>%Tamanho = 0%</li>
+              <li>%Elemento (Oponente) = 0%</li>
+              <li>%Elemento (Ataque) = 0%</li>
+              <li>%Dano Físico = 0%</li>
+              <li>%Atq Arma = 0%</li>
+              <li>%Dano F. Distância = 0%</li>
+              <li>%Dano F. Corpo a Corpo = 0%</li>
+              <li>%Multiplicadores (Grupo A) = 0%</li>
+              <li>%Multiplicadores (Grupo B) = 0%</li>
+              <li>%Dano Crítico = 0%</li>
+              <li>%Habilidade = 0%</li>
+              <li>Grupo A = 0</li>
+              <li>Grupo B = 0</li>
+
+            </ul>
+          </div>
+
+        </div>
       </div>
     </div>
 
